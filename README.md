d5b5aee3875e822e8df20e97b6a6e29890fd2082
yurihallan <yurihallan9@gmail.com>
2021-04-14 18:36:52 -0400

    Projeto pratico de um marketplace feito com vuejs 3 - vue Mastery

---
+++ README.md
@@ -0,0 +1,5 @@
+Mini projeto prático do curso Vue mastery.
+
+Curso intro vue 3.
+
+https://www.vuemastery.com/
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return {
+            product: 'Boots',
+            description: 'Descricao do produto.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://vuejs.org/',
+            inventory:100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:2234, color:'green'},
+                {id:2235, color:'blue'},
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
+        }
+    }
+})
